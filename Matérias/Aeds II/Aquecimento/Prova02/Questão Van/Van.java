b813ef7171b1aa7838a315baac95e37ca0cfc7ce
Vitor <vitormeiragomes@outlook.com>
2024-05-21 19:52:06 -0300

    Questão da Van em java

---
+++ Matérias/Aeds II/Aquecimento/Prova02/Questão Van/Van.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Aluno {
+    int distancia;
+    char coordenada;
+    String nome;
+
+}
+
+
+
+
+public class Van {
+    
+    public static void main(String args[])
+    {
+        Scanner scanf = new Scanner(System.in);
+
+        int qtdAlunos = scanf.nextInt();
+
+        Aluno listas[qtdAlunos];
+
+        ArrayList<Aluno> lista =  new ArrayList<>();
+
+        for(int i = 0; i < qtdAlunos; i++)
+        {
+            Aluno x;
+        }
+
+
+
+        scanf.close();
+
+    }
+}
