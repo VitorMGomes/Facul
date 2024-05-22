d64e55573f3e43a5e3aa338fac624b4cb397f039
Vitor <vitormeiragomes@outlook.com>
2024-05-21 23:24:08 -0300

    Fuçando em java

--- Matérias/Aeds II/Aquecimento/Prova02/Questão Van/Van.java
+++ Matérias/Aeds II/Aquecimento/Prova02/Questão Van/Van.java
@@ -5,31 +5,106 @@ class Aluno {
     char coordenada;
     String nome;
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCoordenada(char coordenada) {
+        this.coordenada = coordenada;
+    }
+
+    public void setDistancia(int distancia) {
+        this.distancia = distancia;
+    }
 }
 
-
-
-
 public class Van {
-    
-    public static void main(String args[])
-    {
+
+    static boolean comparar(Aluno i, Aluno j) {
+        boolean resp = false;
+
+        if (i.distancia == j.distancia) {
+            if (i.coordenada == j.coordenada) {
+                if (i.nome.equals(j.nome)) {
+
+                } else {
+                    if (i.nome.compareTo(j.nome) < 0) {
+                        resp = true;
+                    }
+                }
+            } else {
+                if (i.coordenada > j.coordenada) {
+                    resp = true;
+                }
+            }
+        } else {
+            if (i.distancia > j.distancia) {
+                resp = true;
+            }
+        }
+
+        return resp;
+    }
+
+    public static void main(String args[]) {
         Scanner scanf = new Scanner(System.in);
 
         int qtdAlunos = scanf.nextInt();
 
-        Aluno listas[qtdAlunos];
+        Aluno[] listaM = new Aluno[qtdAlunos]; // criando array de alunos manualmente
 
-        ArrayList<Aluno> lista =  new ArrayList<>();
+        ArrayList<Aluno> listaN = new ArrayList<>(); // usando classe nativa do java
 
-        for(int i = 0; i < qtdAlunos; i++)
-        {
-            Aluno x;
+        for (int i = 0; i < qtdAlunos; i++) {
+            Aluno x = new Aluno();
+
+            String nome = scanf.nextLine();
+            x.setNome(nome);
+
+            char coordenada = scanf.next().charAt(0);
+            x.setCoordenada(coordenada);
+
+            int distancia = scanf.nextInt();
+            x.setDistancia(distancia);
+
+            scanf.nextLine();
+
+            listaM[i] = x;
+
+            listaN.add(x);
         }
 
+        for (int i = 0; i < qtdAlunos - 1; i++) {
+            int menor = i;
 
+            for (int j = i + 1; j < qtdAlunos; j++) {
+                if (comparar(listaM[i], listaM[j])) {
+                    menor = j;
+                }
+            }
 
-        scanf.close();
+            Aluno tmp = listaM[i];
 
+            listaM[i] = listaM[menor];
+
+            listaM[menor] = tmp;
+        }
+
+        for (int i = 0; i < qtdAlunos - 1; i++) {
+            int menor = i;
+
+            for (int j = i + 1; j < qtdAlunos; j++) {
+                if (comparar(listaN.get(i), listaN.get(j))) {
+                    menor = j;
+                }
+            }
+
+            Aluno tmp = listaN.get(i);
+            listaN.set(i, listaN.get(menor));
+            listaN.set(menor, tmp);
+
+            scanf.close();
+
+        }
     }
 }
