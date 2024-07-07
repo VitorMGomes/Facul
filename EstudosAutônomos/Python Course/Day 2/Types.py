72ac564ab9e6221397d908362242223f8934c0e2
Vitor <vitormeiragomes@outlook.com>
2024-07-07 02:06:46 -0300

    Day 2

---
+++ EstudosAutônomos/Python Course/Day 2/Types.py
@@ -0,0 +1,43 @@
+## Data types
+
+#String
+
+print("Hello"[0]) #printa a posicao da string
+#      01234
+
+
+
+print ("123" + "456") #String
+
+print(123 + 456) #Integer
+
+print(123_456_789)
+
+print(3.14159) #Float
+
+
+#Boolean
+True
+False
+
+
+num_char = len(input("What is your name?"))
+# print("Your name has " + num_char + " characters.")
+
+print(type(num_char))
+
+
+print("Your name has " + num_char + " characters.")
+
+
+a = float(123)
+print(type(a))
+
+
+print(round(8/3 , 2))
+print(round(8/3 , 3))
+print(round(8/3 , 4))
+
+
+# //
+
