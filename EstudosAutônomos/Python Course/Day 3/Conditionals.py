f67da7067cb9ae60c266854e4f03bd3c8ddb7d79
Vitor <vitormeiragomes@outlook.com>
2024-07-15 18:18:24 -0300

    Finalizando D3

---
+++ EstudosAutônomos/Python Course/Day 3/Conditionals.py
@@ -0,0 +1,20 @@
+print("Welcome to the rollercoaster!")
+
+height = float(input("What is your height in cm? "))
+
+if (height <= 1.90 and height >= 120):
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry. You cannot ride the rollercoaster...")
+
+# == comparacao == java
+# != 
+# >=
+# <=
+# and == &
+# not == !
+# or == ||
+
+
+numero = int(input("Digite um numero"))
+
