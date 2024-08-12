46cf14d157d5f58958d7efa22bb532174b71ce3a
Vitor <vitormeiragomes@outlook.com>
2024-08-12 17:25:49 -0300

    Sla

---
+++ EstudosAutônomos/C/Capitulo 1/Declaracao.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x; //declaração sem definição de valor
+    printf("x = %d\n", x);
+
+    x = 5;
+    printf("x = %d\n", x);
+
+
+    return 0;
+
+// %c -- character
+// %d ou %i -- int
+// %u -- numeros inteiros unsigned
+// %f -- float or double
+// %s -- varios characteres
+// %p -- endereco de memoria
+// %e ou %E -- notacao ciencitifica
+}
---
+++ EstudosAutônomos/C/Capitulo 1/Entrada.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    scanf("%d", &x);
+    float n;
+    scanf("%f", &n);
+    char y;
+    y = getchar();
+
+}
