033ddd1282ae608d22e6ec831310bd61406f8f65
Vitor <vitormeiragomes@outlook.com>
2024-08-16 18:25:07 -0300

    Q10

---
+++ Matérias/Aeds II/TPs/TP1/Q10/Q10.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Q10 {
+
+    public static boolean stop(String input) {
+        boolean resp = false;
+
+        if (input.length() == 3 && input.charAt(0) == 'F' && input.charAt(1) == 'I' && input.charAt(2) == 'M') 
+        {
+            resp = true;
+        }
+
+        return resp;
+    }
+
+    public static boolean isPal(String input)
+    {
+        return isPal(input, true, input.length(), 0);
+    }
+
+    public static boolean isPal(String input, boolean is, int len, int pos)
+    {
+        if(pos <= len/2)
+        {
+            if(input.charAt(pos) != input.charAt(len - pos - 1))
+            {
+                is = false;
+            }
+
+            return isPal(input, is, len, ++pos);
+        }
+
+        return is;
+    }
+
+    
+
+    public static void main(String args[])
+    {
+        Scanner scanf = new Scanner(System.in);
+
+        for(String input = scanf.nextLine(); !stop(input); input = scanf.nextLine())
+        {
+            if(isPal(input))
+            {
+                System.out.println("SIM");
+            }
+            else
+            {
+                System.out.println("NAO");
+            }
+        }
+        scanf.close();
+    }
+
+
+
+
+}
