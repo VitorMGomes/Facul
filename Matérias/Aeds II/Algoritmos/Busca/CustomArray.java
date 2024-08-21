56030bb005367843b28d0c3840faeff813c47447
Vitor <vitormeiragomes@outlook.com>
2024-08-21 10:16:57 -0300

    Ajustando Pesquisa

--- Matérias/Aeds II/Algoritmos/Busca/CustomArray.java
+++ Matérias/Aeds II/Algoritmos/Busca/CustomArray.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class CustomArray{
 
     private int tam;
@@ -7,13 +9,39 @@ public class CustomArray{
     {
         this.tam = tam;
         this.array = new int[tam];
+        fillArray();
     }
 
-    public fillArray()
+    public void fillArray()
     {
         for(int i = 0; i < tam; i++)
         {
-            this.array[i] = i;
+            this.array[i] = i + 1;
         }
     }
+
+    public void printArray()
+    {
+        System.out.print("|");
+        for(int i = 0; i < tam; i++)
+        {
+            System.out.print(array[i] + "|");
+        }
+        System.out.println();
+    }
+
+    public void customShuffle()
+    {
+        Random random = new Random();
+
+        for(int i = tam - 1; i > 0; i--)
+        {
+            int j = random.nextInt(i + 1);
+
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
 }
--- Matérias/Aeds II/Algoritmos/Busca/Principal.java
+++ Matérias/Aeds II/Algoritmos/Busca/Principal.java
@@ -1,16 +1,51 @@
 import java.util.*;
 
 class Principal {
-    
+    public boolean pesquisaBinaria(CustomArray array)
+    {
+
+    }
+
+    public void pesquisaBinaria(CustomArray array, Scanner scanf)
+    {
+
+        System.out.println("Digite um número (inteiro) para pesquisar no array: ");
+        int pesquisar = scanf.nextInt();
+
+        System.out.println(pesquisaBinaria(array) ? "Está presente no array" : "Não está presente no array;");
+
+    }
+
+    public void pesquisaSequencial(CustomArray array, Scanner scanf)
+    {
+
+    }
+
+
     public static void main(String args[])
     {   
         Scanner scanf = new Scanner(System.in);
         
         System.out.print("Digite o tamanho do array que deseja: ");
         int tam = scanf.nextInt();
-
         CustomArray array = new CustomArray(tam);
 
+        // array.printArray();
+        // array.customShuffle();
+        // array.printArray();
+
+        System.out.println("(1) Pesquisa Sequencial; \n(2) Pesquisa Binária\nDigite qual deseja utilizar: ");
+        int choose = scanf.nextInt();
+        switch(choose)
+        {
+            case 1:
+            pesquisaSequencial(array, scanf);
+
+            case 2:
+            pesquisaBinaria(array, scanf);
+
+            
+        }
         
     }
 }
