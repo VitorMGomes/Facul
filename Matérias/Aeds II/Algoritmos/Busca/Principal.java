1d7aeea84665112569b5ce8d82edf8bc71895cb1
Vitor <vitormeiragomes@outlook.com>
2024-08-21 15:17:48 -0300

    Finalizado algoritimos de busca

--- Matérias/Aeds II/Algoritmos/Busca/Principal.java
+++ Matérias/Aeds II/Algoritmos/Busca/Principal.java
@@ -28,24 +28,83 @@ class Principal {
         comparacoes = 0;
 
         System.out.println("Digite um número (inteiro) para pesquisar no array: ");
-
         int pesquisar = scanf.nextInt();
+
+        System.out.println("//----------------------------------------------//----------------------------------------------//");
         //array.customShuffle();
         array.printArray();
         System.out.println(pesquisaBinaria(array, pesquisar, 0, array.getTam() - 1) ? "Está presente no array" : "Não está presente no array;");
-
         System.out.println("Foram feitas " + comparacoes + " comparações");
 
+        System.out.println("//----------------------------------------------//----------------------------------------------//");
         
+        System.out.println("O custo da busca binária é de lg(n)");
+        System.out.println("Para efetuar a busca binária o array tem de estar ordenado. Todos nessa parte do código estarão em ordem crescente sem elementos repetidos");
+        System.out.println("Caso o array esteja desordenado existe a chance de mesmo o elemento presente ser apontado como faltante");
+
+        System.out.println("//----------------------------------------------//----------------------------------------------//");
+
+        array.customShuffle();
+        array.printArray();
+        comparacoes = 0;
+        System.out.println(pesquisaBinaria(array, pesquisar, 0, array.getTam() - 1) ? "Está presente no array" : "Não está presente no array;");
+        System.out.println("Foram feitas " + comparacoes + " comparações");
 
 
 
     }
 
-    // public void pesquisaSequencial(CustomArray array, Scanner scanf)
-    // {
+    public static boolean pesquisaSequencial(CustomArray array, int pesquisa, int tam)
+    {
+        boolean resp = false;
 
-    // }
+        for(int i = 0; i < tam; i++)
+        {
+            comparacoes++;
+            if(array.array[i] == pesquisa)
+            {
+                resp = true;
+                i = tam;
+            }
+        }
+
+        return resp;
+    }
+
+    public static void pesquisaSequencial(CustomArray array, Scanner scanf)
+    {
+        comparacoes = 0;
+
+        System.out.println("Digite um número(inteiro) para buscar no array");
+        int pesquisar = scanf.nextInt();
+
+        System.out.println("//----------------------------------------------//----------------------------------------------//");
+        //array.customShuffle();
+        array.printArray();
+
+        System.out.println(pesquisaSequencial(array, pesquisar, array.getTam()) ? "Está presente no array" : "Não está presente no array");
+
+        System.out.println("Foram feitas " + comparacoes + " comparações");
+
+        System.out.println("//----------------------------------------------//----------------------------------------------//");
+        
+        System.out.println("O custo da busca sequencial é de teta(n)");
+        System.out.println("Para efetuar a busca sequencial o array pode estar ordenado ou não");
+        System.out.println("É possível alterar o sequencial para contar o número de repetiçoes de um elemento em um array, sendo ele ordenado ou não");
+        System.out.println("É possível otimizar o algoritimo para encerrar as operações quando achar o item pesquisado");
+
+        System.out.println("//----------------------------------------------//----------------------------------------------//");
+        
+        comparacoes = 0;
+
+        array.customShuffle();
+        System.out.println("Pesquisando agora em um array desordenado");
+        
+        System.out.println(pesquisaSequencial(array, pesquisar, array.getTam()) ? "Está presente no array" : "Não está presente no array");
+
+        System.out.println("Foram feitas " + comparacoes + " comparações");
+
+    }
 
 
     public static void main(String args[])
@@ -65,7 +124,7 @@ class Principal {
         switch(choose)
         {
             case 1:
-            //pesquisaSequencial(array, scanf);
+            pesquisaSequencial(array, scanf);
             break;
 
             case 2:
