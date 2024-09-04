9e4781b63f6f114c5a9c9e57e19b39cc33fbdabe
Vitor <vitormeiragomes@outlook.com>
2024-09-04 17:29:59 -0300

    ?

---
+++ Matérias/Aeds II/Beecrowd/Ordenação por Tamanho/C/ord.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+
+#define flag (puts("----***----"))
+
+int main()
+{
+    int entrada;
+    scanf(" %d", &entrada);
+
+    for(int i = 0; i < entrada; i++)
+    {
+        char palavras[50][30];
+        
+            
+    }
+}
