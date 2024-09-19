ad4ea6bc56ec8041297b5739d07ada8b78a038f4
Vitor <vitormeiragomes@outlook.com>
2024-09-19 14:45:48 -0300

    Movendo curso de python para pasta propria

--- EstudosAutônomos/Python Course/Day 1/Input.py
+++
@@ -1,7 +0,0 @@
-input("What is your name?")
-input("What is your name? \n") #input/entrada -- scanf (c)/ scanner (java) -- ja vem com a mensagem de print imbutida
-
-print("What is your age?")
-input()
-
-print("Hello " + input("What is your name?") + "!") 
--- EstudosAutônomos/Python Course/Day 1/Z1.py
+++
@@ -1,9 +0,0 @@
-name = input("What is your name? \n")
-
-dogs_name = input("What is your dogs name?\n")
-
-print(name + " " + dogs_name)
-
-
-#jeitinho esquisito
-print(input("What is your name? \n") + " " + input("What is your dogs name?\n"))
