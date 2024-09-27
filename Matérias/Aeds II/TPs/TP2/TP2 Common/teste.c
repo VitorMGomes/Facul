6f1bfb9c0912ba65c07cc0058a515d2255b4e3b1
Vitor <vitormeiragomes@outlook.com>
2024-09-27 02:15:53 -0300

    Ajustando teste C

--- Matérias/Aeds II/TPs/TP2/TP2 Common/teste.c
+++ Matérias/Aeds II/TPs/TP2/TP2 Common/teste.c
@@ -5,38 +5,132 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-// typedef struct Date
-// {
-//     int dia, mes, ano;
-// } Date;
+typedef struct Date
+{
+    int dia, mes, ano;
+} Date;
 
-// typedef struct List
-// {    
-//     char element[30];
-// }List;
-
-
-// typedef struct Pokemon
-// {
-//     int id;
-//     int generation;
-//     char name[20];
-//     char description[40];
-//     List types[2];
-//     List abilities[6];
-//     double weight;
-//     double height;
-//     int captureRate;
-//     bool legendary;
-//     Date captureDate;
+typedef struct Pokemon
+{
+    int id;
+    int generation;
+    char name[20];
+    char description[40];
+    char types[50];
+    char abilities[60];
+    double weight;
+    double height;
+    int captureRate;
+    bool legendary;
+    Date captureDate;
 
 
-// } Pokemon;
+} Pokemon;
 
-// void startPoke()
-// {
+void startPoke()
+{
     
-// }
+}
+
+
+void freeSplit(char **array)
+{
+    int i;
+    for (i = 0; strcmp(array[i], "cFIM"); i++)
+    {
+        free(array[i]);
+    }
+
+    free(array[i]);
+
+    free(array);
+}
+
+
+
+char **split(char *regex, char *string)
+{
+
+    int len = strlen(string);
+    int n = 1;
+
+    for (int i = 0; i < len; i++)
+    {
+        if (string[i] == regex[0])
+            n++;
+    }
+
+    char **array = (char **)malloc((n + 1) * sizeof(char *));
+
+    for (int i = 0; i < n + 1; i++)
+    {
+        array[i] = calloc(200, sizeof(char *));
+    }
+
+    strcpy(array[n], "cFIM");
+
+    strcpy(array[0], strsep(&string, regex));
+
+    for (int i = 1; i < n; i++)
+    {
+        char *temp = strsep(&string, regex);
+        strcpy(array[i], temp);
+    }
+
+    return array;
+}
+
+Pokemon ler(char *string)
+{
+
+    // char aux[2000];
+    // strcpy(aux, string);
+    char **array = split(";", string);
+
+    int pos = 0;
+    Pokemon x;
+
+    setId(array[pos++], &x);
+
+    setName(array[pos++], &x);
+
+    setAlternativeNames(array[pos++], &x);
+
+    setHouse(array[pos++], &x);
+
+    setAncestry(array[pos++], &x);
+
+    setSpecies(array[pos++], &x);
+
+    setPatronus(array[pos++], &x);
+
+    setHogwartsStaff(array[pos++], &x);
+
+    setHogwartsStudent(array[pos++], &x);
+
+    setActorName(array[pos++], &x);
+
+    setAlive(array[pos++], &x);
+
+    // Para pular o atributo não usado
+    pos++;
+
+    setDateOfBirth(array[pos++], &x);
+
+    setYearOfBirth(array[pos++], &x);
+
+    setEyeColour(array[pos++], &x);
+
+    setGender(array[pos++], &x);
+
+    setHairColour(array[pos++], &x);
+
+    setWizard(array[pos++], &x);
+
+    freeSplit(array);
+
+    return x;
+}
 
 void handleLine(char *line)
 {
