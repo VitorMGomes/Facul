23c3e837d11768fc6e60c83d4ed9821b5076ccf0
Vitor <vitormeiragomes@outlook.com>
2024-10-22 11:56:27 -0300

    Ajustes

--- Matérias/Aeds II/TPs/TP3/TP03Q01 - Lista com Alocação Sequencial em Java/Main.java
+++ Matérias/Aeds II/TPs/TP3/TP03Q01 - Lista com Alocação Sequencial em Java/Main.java
@@ -16,9 +16,10 @@ class Pokemon {
     private int captureRate;
     private boolean legendary;
     private Date captureDate;
-    //private String captureDate;
+    // private String captureDate;
 
-    // -------------------------------------------------------------- Construtor Vazio -------------------------------------------------------------- //    
+    // -------------------------------------------------------------- Construtor
+    // Vazio -------------------------------------------------------------- //
     public Pokemon() {
         this.id = 0;
         this.generation = 0;
@@ -32,7 +33,9 @@ class Pokemon {
         this.legendary = false;
         this.captureDate = new Date();
     }
-    // -------------------------------------------------------------- Construtor -------------------------------------------------------------- //
+
+    // -------------------------------------------------------------- Construtor
+    // -------------------------------------------------------------- //
     public Pokemon(int id, int generation, String name, String description, ArrayList<String> types,
             ArrayList<String> abilities,
             Double weight,
@@ -50,30 +53,97 @@ class Pokemon {
         this.captureDate = captureDate;
     }
 
-    // -------------------------------------------------------------- Getters & Setters -------------------------------------------------------------- //
-    public void setAbilities(ArrayList<String> abilities) { this.abilities = abilities; }
-    public ArrayList<String> getAbilities() { return abilities; }
-    public void setCaptureDate(Date captureDate) { this.captureDate = captureDate; }
-    public Date getCaptureDate() { return captureDate; }
-    public void setCaptureRate(int captureRate) { this.captureRate = captureRate; }
-    public int getCaptureRate() { return captureRate; }
-    public void setDescription(String description) { this.description = description; }
-    public String getDescription() { return description; }
-    public void setGeneration(int generation) { this.generation = generation; }
-    public int getGeneration() { return generation; }
-    public void setHeight(Double height) { this.height = height; }
-    public Double getHeight() { return height; }
-    public void setId(int id) { this.id = id; }
-    public int getId() { return id; }
-    public void setLegendary(boolean legendary) { this.legendary = legendary; }
-    public boolean isLegendary() { return legendary; }
-    public void setName(String name) { this.name = name; }
-    public String getName() { return name; }
-    public void setTypes(ArrayList<String> types) { this.types = types; }
-    public ArrayList<String> getTypes() { return types; }
-    public void setWeight(Double weight) { this.weight = weight; }
-    public Double getWeight() { return weight; }
-    // -------------------------------------------------------------- Getters & Setters -------------------------------------------------------------- //
+    // -------------------------------------------------------------- Getters &
+    // Setters -------------------------------------------------------------- //
+    public void setAbilities(ArrayList<String> abilities) {
+        this.abilities = abilities;
+    }
+
+    public ArrayList<String> getAbilities() {
+        return abilities;
+    }
+
+    public void setCaptureDate(Date captureDate) {
+        this.captureDate = captureDate;
+    }
+
+    public Date getCaptureDate() {
+        return captureDate;
+    }
+
+    public void setCaptureRate(int captureRate) {
+        this.captureRate = captureRate;
+    }
+
+    public int getCaptureRate() {
+        return captureRate;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setGeneration(int generation) {
+        this.generation = generation;
+    }
+
+    public int getGeneration() {
+        return generation;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setLegendary(boolean legendary) {
+        this.legendary = legendary;
+    }
+
+    public boolean isLegendary() {
+        return legendary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setTypes(ArrayList<String> types) {
+        this.types = types;
+    }
+
+    public ArrayList<String> getTypes() {
+        return types;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+    // -------------------------------------------------------------- Getters &
+    // Setters -------------------------------------------------------------- //
 
     public Pokemon myClone() {
         Pokemon clonado = new Pokemon(id, generation, name, description, types, abilities,
@@ -83,7 +153,8 @@ class Pokemon {
         return clonado;
     }
 
-    // -------------------------------------------------------------- Formatando Types -------------------------------------------------------------- //
+    // -------------------------------------------------------------- Formatando
+    // Types -------------------------------------------------------------- //
     private ArrayList<String> formatTypes(String type1, String type2) {
         ArrayList<String> types = new ArrayList<>();
         if (type1 != "") {
@@ -97,7 +168,8 @@ class Pokemon {
         return types;
     }
 
-    // -------------------------------------------------------------- Formatando Abilidades -------------------------------------------------------------- //
+    // -------------------------------------------------------------- Formatando
+    // Abilidades -------------------------------------------------------------- //
     private ArrayList<String> formatAbilitieList(String listAbilities) {
         ArrayList<String> abilities = new ArrayList<>();
 
@@ -114,7 +186,7 @@ class Pokemon {
         boolean insideQuotes = false;
         int tam = line.length();
 
-        //substituindo as virgulas que separam o as colunas do csv por ";"
+        // substituindo as virgulas que separam o as colunas do csv por ";"
         for (int i = 0; i < tam; i++) {
             char currentChar = line.charAt(i);
 
@@ -165,7 +237,7 @@ class Pokemon {
 
             SimpleDateFormat formater = new SimpleDateFormat("dd/MM/yyyy");
             setCaptureDate(formater.parse(splitted[11]));
-        
+
         } catch (NumberFormatException e) {
             System.err.println("Erro ao converter número: " + e.getMessage());
         } catch (ParseException e) {
@@ -184,16 +256,16 @@ class Pokemon {
 
         Pokemon pokemons[] = new Pokemon[801];
         Scanner reader = null;
-    
+
         try {
             reader = new Scanner(new FileReader("/tmp/pokemon.csv"));
-    
+
             reader.nextLine();
-    
+
             for (int i = 0; reader.hasNextLine(); i++) {
                 String line = reader.nextLine();
                 Pokemon pokemon = new Pokemon();
-    
+
                 try {
                     pokemon.ler(line);
                     pokemons[i] = pokemon;
@@ -201,7 +273,7 @@ class Pokemon {
                     System.err.println("Erro ao processar linha " + (i + 1) + ": " + e.getMessage());
                 }
             }
-    
+
         } catch (FileNotFoundException e) {
             System.err.println("Arquivo não encontrado: " + e.getMessage());
         } catch (Exception e) {
@@ -211,7 +283,7 @@ class Pokemon {
                 reader.close();
             }
         }
-    
+
         return pokemons;
     }
 
@@ -225,21 +297,18 @@ class Pokemon {
 
 }
 
-class Lista{
+class Lista {
 
     private int tam;
     private Pokemon array[];
 
-    Lista(int maxTam)
-    {
+    Lista(int maxTam) {
         tam = 0;
         array = new Pokemon[maxTam];
     }
 
-    public void inserirFim(Pokemon x) throws Exception
-    {
-        if(tam == array.length)
-        {
+    public void inserirFim(Pokemon x) throws Exception {
+        if (tam == array.length) {
             throw new Exception("Lista cheia! Erro ao inserir no fim");
         }
 
@@ -248,15 +317,12 @@ class Lista{
 
     }
 
-    public void inserirInicio(Pokemon x) throws Exception
-    {
-        if(tam == array.length)
-        {
+    public void inserirInicio(Pokemon x) throws Exception {
+        if (tam == array.length) {
             throw new Exception("Lista cheia! Erro ao inserir no inicio");
         }
 
-        for(int i = tam; i > 0; i--)
-        {
+        for (int i = tam; i > 0; i--) {
             array[i] = array[i - 1];
         }
 
@@ -264,83 +330,107 @@ class Lista{
         tam++;
     }
 
-    public void inserir(Pokemon x, int pos) throws Exception
-    {
-        if(tam == array.length )
-        {
+    public void inserir(Pokemon x, int pos) throws Exception {
+        if (tam == array.length) {
             throw new Exception("Lista cheia! Erro ao inserir");
         }
-        if(pos > tam || pos < 0)
-        {
+        if (pos > tam || pos < 0) {
             throw new Exception("Posição inválida para inserir");
-        }
-        else if(pos == 0)
-        {
+        } else if (pos == 0) {
             inserirInicio(x);
-        }
-        else if(pos == tam)
-        {
+        } else if (pos == tam) {
             inserirFim(x);
         }
 
-        for(int i = tam; i > pos; i--)
-        {
-            array[i] = array[i-1];
+        for (int i = tam; i > pos; i--) {
+            array[i] = array[i - 1];
         }
 
         array[pos] = x;
         tam++;
     }
-    
-    public Pokemon remover(int pos) throws Exception
-    {
-        if(tam == 0)
-        {
+
+    public Pokemon remover(int pos) throws Exception {
+        if (tam == 0) {
             throw new Exception("Lista vazia! Erro ao remover");
-        }
-        else if(pos < 0 || pos > tam)
-        {
+        } else if (pos < 0 || pos > tam) {
             throw new Exception("Posição inválida! Erro ao remover");
         }
 
         Pokemon resp = array[pos];
-        tam--; //se fizer isso depois do shift de elementos a condicao tem de ser (i < tam - 1)
+        tam--; // se fizer isso depois do shift de elementos a condicao tem de ser (i < tam -
+               // 1)
 
-        for(int i = pos; i < tam; i++)
-        {
-            array[i] = array[i + 1];
-        }
-    }
-
-    public Pokemon removerInicio() throws Exception
-    {
-        if(tam == 0)
-        {
-            throw new Exception("Lista vazia! Erro ao remover no inicio");
-        }
-
-        Pokemon resp = array[0];
-        tam--;
-
-        for(int i = 0; i < tam; i++)
-        {
+        for (int i = pos; i < tam; i++) {
             array[i] = array[i + 1];
         }
 
         return resp;
     }
 
-    
+    public Pokemon removerInicio() throws Exception {
+        if (tam == 0) {
+            throw new Exception("Lista vazia! Erro ao remover no inicio");
+        }
+
+        Pokemon resp = array[0];
+        tam--;
+
+        for (int i = 0; i < tam; i++) {
+            array[i] = array[i + 1];
+        }
+
+        return resp;
+    }
+
+    public Pokemon removerFim() throws Exception {
+        if (tam == 0) {
+            throw new Exception("Lista vazia! Erro ao remover no fim");
+        }
+
+        Pokemon resp = array[tam - 1];
+        tam--;
+
+        return resp;
+    }
+
+    public void mostrar()
+    {
+        for(int i = 0; i < tam; i++)
+        {
+            array[i].mostrar();
+        }
+    }
+
 }
 
-public class Main 
-{
-    
-    public static Pokemon[] fullDB = new Pokemon[801];
-    public static Pokemon[] array;
+public class Main {
 
-    public static void main(String args[])
+    public static Pokemon[] fullDB = new Pokemon[801];
+
+    public static void operacao(String[] input)
     {
+        if(input.length == 1)
+        {
+
+        }
+        else if(input.length == 2)
+        {
+            
+        }
+        else if(input.length == 3)
+        {
+
+        }
+        else
+        {
+            System.out.println("Erro no input");
+        }
+    }
+
+    public static void main(String args[]) {
+        Lista list = new Lista(100);
+
         fullDB = Pokemon.readDb();
 
         Scanner scanf = new Scanner(System.in);
@@ -348,21 +438,35 @@ public class Main
         int numeros[] = new int[100];
         int len = 0;
 
-        for(String input = scanf.nextLine();!input.equals("FIM"); input = scanf.nextLine())
-        {
+        for (String input = scanf.nextLine(); !input.equals("FIM"); input = scanf.nextLine()) {
             numeros[len] = Integer.parseInt(input);
-            len++;    
+            len++;
         }
 
-        array = new Pokemon[len];
+        try {
 
-        for(int i = 0; i < len; i++)
+            for (int i = 0; i < len; i++) {
+                list.inserirFim(fullDB[numeros[i] - 1].myClone());
+            }
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        
+        int num = scanf.nextInt();
+        
+        for(int i = 0; i < num; i++)
         {
-            array[i] = fullDB[numeros[i] - 1].myClone();
+            String input = scanf.nextLine();
+            String[] splitted = input.split(" ");
+            operacao(splitted);
+
         }
-
-
-
+        
+        
+        list.mostrar();
+        
         scanf.close();
 
     }
